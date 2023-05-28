238816debcb24913e18f6b74c8ce279f16dddc6b
mikhailusachev <mikhailusachev@users.noreply.github.com>
2023-05-28 21:24:42 +0300

    Update prod.clj

--- src/prod.clj
+++ src/prod.clj
@@ -8,6 +8,7 @@
   {:host "0.0.0.0", :port 8080, :resources-path "public"})
 
 (defn -main [& args] ; run with `clj -M -m prod`
+  (System/setProperty "HYPERFIDDLE_ELECTRIC_SERVER_VERSION" "cd8e92b-dirty")
   (when (clojure.string/blank? (System/getProperty "HYPERFIDDLE_ELECTRIC_SERVER_VERSION"))
     (throw (ex-info "HYPERFIDDLE_ELECTRIC_SERVER_VERSION jvm property must be set in prod" {})))
   (electric-server-java8-jetty9/start-server! electric-server-config))
